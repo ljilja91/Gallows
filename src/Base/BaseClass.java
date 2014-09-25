27a6b17e674344b681fc98a0020901eab64fc394
Ljiljana <>
2014-09-25 15:47:31 +0200

    Kreirani fajlovi sa riječima i osnovna klasa.

---
+++ src/Base/BaseClass.java
@@ -0,0 +1,26 @@
+
+package Base;
+
+/**
+ *
+ * @author Ljiljana
+ */
+public class BaseClass 
+{
+    protected String word;
+
+    /**
+     * @return the word
+     */
+    public String getWord() {
+        return word;
+    }
+
+    /**
+     * @param word the word to set
+     */
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
+
