494c4f925985332aae156fa93e019e028c1b8a9d
Ljiljana <>
2014-09-25 23:07:49 +0200

    Dorađivanje frame klase, ne radi.

--- src/gallows/Frame.java
+++ src/gallows/Frame.java
@@ -1,9 +1,15 @@
 package gallows;
 
+import Base.BaseClass;
+import Panels.DownPanel;
+import Panels.UpPanel;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.MenuBar;
 import java.awt.Toolkit;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
 
 /**
@@ -12,8 +18,22 @@ import javax.swing.WindowConstants;
  */
 public class Frame extends JFrame
 {
-  
+    private DownPanel downPanel;
+    private UpPanel upPanel;
+    
+    private MenuBar meni;
+    private JPanel mainPanel;
+    private Font customFont = null;
+    private BaseClass base;
+    
+    
     public Frame(Font font)
+    {
+        this.customFont = font;
+        this.frameSettings();
+    }
+    
+    private void frameSettings() 
     {
         this.setTitle("Gallows");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -22,7 +42,7 @@ public class Frame extends JFrame
         
         Dimension dimensionWindow = new Dimension(500, 400);
         
-        this.setSize(500, 400);
+        this.setSize(mainPanel.getWidth(), mainPanel.getHeight());
         
         Toolkit tkDimension = Toolkit.getDefaultToolkit();
 	Dimension dimensionScrean = tkDimension.getScreenSize();
@@ -31,6 +51,7 @@ public class Frame extends JFrame
 				dimensionScrean.height / 2 - dimensionWindow.height / 2);
         
         this.pack();
+        this.setIconImage(new ImageIcon("src/Content/Images/gallowss.png").getImage());
         this.setVisible(true);
     }
 
